334aa33482426ac48c165e9336900b9716ee72d1
Francis <yiyboy@qq.com>
2020-06-15 18:51:02 +0800

    ✅初始化

---
+++ .storybook/preview.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { addDecorator } from "@storybook/react";
+import {ThemeProvider} from 'styled-components'
+import theme from '../src/theme'
+addDecorator(storyFn=>{
+  <ThemeProvider theme={}>
+    {}
+  </ThemeProvider>
+})
