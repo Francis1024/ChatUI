581837a96b2be533e3872919a9a7166e9823a527
Francis <yiyboy@qq.com>
2020-06-16 17:40:12 +0800

    📦 打包生产静态预览文件

---
+++ storybook-static/precache-manifest.3e3ed33ef20061d06919d64b0d082ae7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "594ce00be1ad0171dcf4bed909c3dc4e",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.66db00107e854b508b5b.bundle.js"
+  },
+  {
+    "url": "runtime~main.66db00107e854b508b5b.bundle.js"
+  },
+  {
+    "revision": "101a35279d15af93ffd4",
+    "url": "static/css/main.2fe15962.chunk.css"
+  },
+  {
+    "url": "vendors~main.66db00107e854b508b5b.bundle.js"
+  },
+  {
+    "url": "vendors~main.66db00107e854b508b5b.bundle.js.LICENSE.txt"
+  }
+]);
